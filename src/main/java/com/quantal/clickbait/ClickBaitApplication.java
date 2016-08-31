8a58f97613376a6a16235511bf639d2aedee4fe0
quophyie <quophyie@yahoo.com>
2016-08-31 03:14:45 +0100

    Initial commit

---
+++ src/main/java/com/quantal/clickbait/ClickBaitApplication.java
@@ -0,0 +1,12 @@
+package com.quantal.clickbait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickBaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickBaitApplication.class, args);
+	}
+}
